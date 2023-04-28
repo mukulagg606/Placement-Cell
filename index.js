5d6508ab952eda8e3dfc0fad06bb17dfbe3c8051
Mukul AggArwal <115641523+mukulagg606@users.noreply.github.com>
2023-04-28 11:02:55 +0530

    created placement-cell

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+const app = express();
+
+// set ejs as view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(
+  session({
+    secret: "mukul",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { expiry: 1000 * 60 * 60 },
+  })
+);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+app.use(express.urlencoded({ extended: true }));
+
+// using passport for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// using express router
+app.use('/', require('./routes'));
+
+app.listen(8000, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`App is live on http://localhost:8000`);
+});
+
